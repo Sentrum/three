0eb4dd83be49812c82e53a436fd36e0c62dbc6fb
Sentrum <81400286+Sentrum@users.noreply.github.com>
2022-05-02 15:54:38 +0200

    changes to p4 and working on p1

--- Problem_1.py
+++ Problem_1.py
@@ -22,7 +22,7 @@ def multiplication(A,B,irr):
     result = [ ]
     for i in range(len(A)):
         result.append(0)
-    
+
     for i in range(len(A)):
         if B[i] == 1:
             shift = A
@@ -57,23 +57,23 @@ text = "This is an example input file created specifically for the third mandato
 def text_to_binary_list(P):
     bin_list = []
     for x in P:
-        z = format(ord(x), 'b')   
+        z = format(ord(x), 'b')
         for y in z:
             bin_list.append(int(y))
     #print(bin_list, len(bin_list))
-    return bin_list  
+    return bin_list
 
 
 ### TESTING
 #chr(84) #num to string
 #print(ord("T")) #string to num
-#print(format(84,'b')) #num to binary 
+#print(format(84,'b')) #num to binary
 
 #text_to_binary_list(text)
 #print(ecb_encrypt(text,key))
 
 
-#   ECB mode: encrypt the plaintext with the same key, and return the ciphertext. 
+#   ECB mode: encrypt the plaintext with the same key, and return the ciphertext.
 def ecb_encrypt(P ,K):
     newP = text_to_binary_list(P) # turn P from a string to a list of bits
     ciphertext = []
@@ -82,7 +82,7 @@ def ecb_encrypt(P ,K):
         ciphertext.append(encrypt(newP[i:i+32], K)) # encrypt the 32 bits of P
         #count += 1
     #print(count) ## for-loop is looping 41 times which is the expected amount of blocks
-    return ciphertext 
+    return ciphertext
 
 
 #   CBC mode: encrypt the plaintext with the same key, and XOR the ciphertext with
@@ -101,22 +101,22 @@ def cbc_encrypt(P, K, IV):
     return ciphertext
 
 
-#function for output feedback mode(OFB) of operation 
+#function for output feedback mode(OFB) of operation
 ## TODO NOT IMPLEMENTED YET
 def ofb_encrypt(P, K, IV):
+    #Output feedback (OFB) mode of operation
+
     newP = text_to_binary_list(P) # turn P from a string to a list of bits
     ciphertext = []
-    prev_ciphertext = IV
-    #count = 0
-    for i in range(0, len(newP), 32):
-        ciphertext.append(encrypt(xor(newP[i:i+32], prev_ciphertext), K)) # encrypt the 32 bits of P
-        prev_ciphertext = ciphertext[-1]
-        #count += 1
-    #print(count) ## for-loop is looping 41 times which is the expected amount of blocks
+    IV =
+
+
     return ciphertext
 
-##TODO 
+##TODO
 # encrypt the plaintext in gold_plaintext.in with ECB, CBC and OFB.
+#can take read file code from files
+
 
 
 
